dfcddb9430823bcd3ddff63c7df2ef42a5988c46
Alex Richardson <alexrichardson@macbook-pro-69.local>
2018-09-12 10:05:32 -0500

    Initial Commit

---
+++ Task Manager/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Task Manager
+//
+//  Created by Alex Richardson on 9/12/18.
+//  Copyright © 2018 Alex Richardson. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
